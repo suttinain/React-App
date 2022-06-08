c58af34bda8bcede1d7e4720d48bd82247240674
snab <79553984+snabhub@users.noreply.github.com>
2022-06-09 00:19:16 +0700

    log in

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Alert, Button, Image } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput>Let's start</TextInput>
-      <StatusBar style="auto" />
+      <View style={styles.form}>
+      <Image source={require('./doge.png')}/>
+      <Text style={styles.title}>DogeCoin</Text>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Username"
+        style={styles.input}
+        keyboardType="email-address"
+        value={email}
+        onChangeText={
+        (text) => setEmail(text)
+      }
+      />
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Password"
+        style={styles.input}
+        keyboardType="default"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button
+        title="Log in"
+        onPress={() => console.log(email, password)}
+      />
+      <Button
+        color = "green"
+        title="Register"
+        onPress={() => console.log(email, password)}
+      />
+      </View>
     </SafeAreaView>
   );
 }
@@ -17,4 +49,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  form: {
+    width: "30%",
+    marginTop: "0%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#057dfe",
+    paddingBottom: 5,
+    fontSize: 20,
+    margin: 10
+  },
+  title:{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    paddingBottom: 30,
+    fontSize: 40,
+    margin: 10
+  }
 });
+export default App;
