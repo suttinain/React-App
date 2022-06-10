8e0cc932a33c7051a4d9b54d892e2c63f882e28f
snab <79553984+snabhub@users.noreply.github.com>
2022-06-10 21:10:13 +0700

    Navigation

--- App.js
+++ App.js
@@ -1,73 +1,24 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Alert, Button, Image } from 'react-native';
-import React, {useState} from 'react';
-
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.form}>
-      <Image source={require('./doge.png')}/>
-      <Text style={styles.title}>DogeCoin</Text>
-      <TextInput
-        autoCapitalize="none"
-        placeholder="Username"
-        style={styles.input}
-        keyboardType="email-address"
-        value={email}
-        onChangeText={
-        (text) => setEmail(text)
-      }
-      />
-      <TextInput
-        autoCapitalize="none"
-        placeholder="Password"
-        style={styles.input}
-        keyboardType="default"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button
-        title="Log in"
-        onPress={() => console.log(email, password)}
-      />
-      <Button
-        color = "green"
-        title="Register"
-        onPress={() => console.log(email, password)}
-      />
-      </View>
-    </SafeAreaView>
-  );
+import React from 'react';
+import { createStackNavigator }
+    from '@react-navigation/stack';
+import { NavigationContainer }
+    from '@react-navigation/native';
+import Login from './screens/Login';
+import Register from './screens/Register';
+const Stack = createStackNavigator();
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={Register}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  form: {
-    width: "30%",
-    marginTop: "0%",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#057dfe",
-    paddingBottom: 5,
-    fontSize: 20,
-    margin: 10
-  },
-  title:{
-    textAlign: 'center',
-    fontWeight: 'bold',
-    paddingBottom: 30,
-    fontSize: 40,
-    margin: 10
-  }
-});
-export default App;
