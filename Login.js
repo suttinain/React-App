bb2028885f325cb1aea023bcdfdf7d1f4bcf6159
snab <79553984+snabhub@users.noreply.github.com>
2022-06-11 14:52:14 +0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,63 +1,74 @@
 import React, {useState} from 'react'
-import { SafeAreaView, Text, StyleSheet, Button, StatusBar, TextInput, View }
+import { SafeAreaView, Text, StyleSheet, Button, StatusBar, TextInput }
     from 'react-native'
-const MainPage = ({route}) => {
-    return(
+import {openDatabase} from 'react-native-sqlite-storage';
+const Login = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.wel}>Welcome {route.params.paramKey}!</Text>
-            <View
-            style={{
-                flexDirection: "row",
-                justifyContent: 'space-around',
-                flexWrap: 'wrap',
-                padding: 20,
-            }}
-            >
-            <View style={{ backgroundColor: "powderblue", width: 300, height: 300, flex: 0.4, justifyContent: 'center' }}>
-                <Text style={styles.s1}>Profile</Text>
-            </View>
-            <View style={{ backgroundColor: "lightgreen", width: 300, height: 300, flex: 0.4, justifyContent: 'center'  }}>
-                <Text style={styles.s1}>Search for Recipes</Text>
-            </View>
-            </View>
-            <View
-            style={{
-                flexDirection: "row",
-                justifyContent: 'space-around',
-                flexWrap: 'wrap',
-                padding: 20,
-            }}
-            >
-            <View style={{ backgroundColor: "lightgreen", width: 300, height: 300, flex: 0.4, justifyContent: 'center' }}>
-                <Text style={styles.s1}>My Recipes</Text>
-            </View>
-            <View style={{ backgroundColor: "powderblue", width: 300, height: 300, flex: 0.4, justifyContent: 'center'  }}>
-                <Text style={styles.s1}>Settings</Text>
-            </View>
-            </View>
+            <Text style={styles.title}>DogeCoin</Text>
+            <TextInput
+                autoCapitalize="none"
+                placeholder="Username"
+                style={styles.input}
+                keyboardType="default"
+                value={username}
+                onChangeText={
+                (text) => setUsername(text)
+            }
+            />
+            <TextInput
+                autoCapitalize="none"
+                placeholder="Password"
+                style={styles.input}
+                keyboardType="default"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button
+                title="Register"
+                onPress={
+                () => navigation.navigate('Register')
+            }
+            />
+            <Button
+                title="Log in"
+                onPress={() => navigation.navigate('MainPage', {paramKey: 'Null'})}
+            />
         </SafeAreaView>
     )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'pink',
-        justifyContent: 'flex-start',
+        backgroundColor: 'lightyellow',
+        alignItems: 'center',
+        justifyContent: 'center',
         paddingTop: StatusBar.currentHeight,
+        borderColor: 'black'
     },
-    wel:{
-        fontSize: 20,
-        color: "grey",
-        alignSelf: 'flex-start',
-        padding: 10,
-        fontWeight: 'bold',
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#057dfe",
+        paddingBottom: 5,
+        fontSize: 15,
+        margin: 10,
+        padding: 15,
+        textAlign: 'left',
     },
-    s1:{
-        fontSize: 20,
-        color: "yellow",
-        alignSelf: 'center',
-        padding: 10,
+    title:{
+        textAlign: 'center',
         fontWeight: 'bold',
+        paddingBottom: 30,
+        fontSize: 40,
+        margin: 10,
+        color: 'gold'
+    },
+    main:{
+        borderColor: 'black',
     }
 });
-export default MainPage;
+export default Login;
